d41a4f24e7bc0dde389e0609e50168a067f6e2e8
Fernando Zurita <ferzuritag@hotmail.com>
2021-11-04 17:31:51 +0100

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function index(){
+        return "<h1>Que onda perro</h1>";
+    }
+    public function forgot(){
+        return "<h1>Que onda perro</h1>";
+    }
+}
